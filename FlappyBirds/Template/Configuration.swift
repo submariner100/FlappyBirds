84b58bbb852398738a056899cde2a0f6b6948153
Michael Chapman <submariner100@gmail.com>
2018-01-02 21:18:18 +0000

    fifth commit

--- FlappyBirds/Template/Configuration.swift
+++ FlappyBirds/Template/Configuration.swift
@@ -11,6 +11,8 @@ import CoreGraphics
 struct ZPosition {
 	static let background: CGFloat = 0
 	static let obstacles: CGFloat = 1
+	static let hudBackground: CGFloat = 10
+	static let hudLabel: CGFloat = 11
 }
 
 struct PhysicsCategory {
